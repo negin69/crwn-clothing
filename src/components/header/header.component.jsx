b5a0eac2e33586bf44ed8fa762c6f6a4fa9c33fe
negin69 <negin_mtn@yahoo.com>
2020-09-07 16:59:13 +0430

    added cartIcon and cart-Dropdown components to our application also implemented cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,8 +3,10 @@ import  "../header/header.styles.scss";
 import {Link} from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/4.1 crown.svg';
 import { auth } from './../../firebase/firebase.utils';
+import CartIcon from './../cart-icon/cart-icon.component';
 import {connect} from 'react-redux';
-const Header = ({currentUser}) => {
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+const Header = ({currentUser , hidden}) => {
     return ( 
         <div className="header">
          <Link className="logo-container"  to="/">
@@ -20,17 +22,26 @@ const Header = ({currentUser}) => {
           </Link>
 
           {currentUser  ?  (<div className="option" onClick={ ()  => auth.signOut () } > Sign Out</div> ) :
-        <Link className="option" to="/signin"> Sign In</Link>  
+       (<Link className="option" to="/signin"> Sign In</Link>  ) 
         }
+
+        <CartIcon  />
          </div>
-         
+         {
+              hidden ? null : <CartDropdown />
+         }
+     
         </div>
      );
 }
 
 
-const  mapStateToProps= state => ({
-  currentUser : state.user.currentUser
-})
+// const  mapStateToProps= state => ({
+//   currentUser : state.user.currentUser
+// })
+
+const  mapStateToProps= ({user : {currentUser} , cart :{hidden} }) => ({
+  currentUser , hidden
+});
 
 export default connect (mapStateToProps)(Header);
---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,13 @@
+// const { default: CartActionTypes } = require("./cart.types");
+
+import CartActionTypes from './cart.types';
+export const toggleCartHidden = () => ({
+type : CartActionTypes.TOGGLE_CART_HIDDEN
+}) ;
+
+// import CartActionTypes from './cart.types';
+
+// export const toggleCartHidden = () => ({
+//   type: CartActionTypes.TOGGLE_CART_HIDDEN
+// });
+
